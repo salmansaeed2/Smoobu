a79e7dc00ce9b1b26c0d09da3447d719f1e8b607
cubixsol <api.cubixsol@gmail.com>
2022-01-03 16:28:39 +0500

    Plugin Added

---
+++ smoobu-plugin/smoobu-plugin.php
@@ -0,0 +1,27 @@
+<?php
+ 
+/**
+ 
+ * @package Akismet
+ 
+ */
+ 
+/*
+ 
+Plugin Name: Smoobu
+ 
+Plugin URI: https://cubixsol.com/
+ 
+Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from spam</strong>. It keeps your site protected even while you sleep. To get started: activate the Akismet plugin and then go to your Akismet Settings page to set up your API key.
+ 
+Version: 5.1.7
+ 
+Author: Cubixsol
+ 
+Author URI: https://cubixsol.com
+ 
+License: GPLv2 or later
+ 
+Text Domain: boostly
+ 
+*/
