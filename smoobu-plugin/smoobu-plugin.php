863f8f464810d5f8a8aa1ee86acfbe0665efb791
salmansaeed2 <m.salmanxaeed@gmail.com>
2022-01-05 13:29:58 +0500

    git header added again

--- smoobu-plugin/smoobu-plugin.php
+++ smoobu-plugin/smoobu-plugin.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * Plugin Name: Smoobu Plugin
- * Github plugin URI:  https://github.com/salmansaeed2/Smoobu
- * github plugin URI: https://github.com/salmansaeed2/Smoobu.git
+ * Github Plugin URI: https://github.com/salmansaeed2/Smoobu.git
+ * Primary Branch: main
  * Description: Manage your real estate business and market your portfolio
  * Version: 3.0.1
  * Author: CubixSol
  * Author URI: https://cubixsol.com
  * Text Domain: Smoobu
  * @package Akismet
- * Github plugin URI:  salmansaeed2/Smoobu
+ 
  
  */
 
