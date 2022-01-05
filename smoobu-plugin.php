99d9e8025ac68d236b440d2dcd8522a8b6ffb460
salmansaeed2 <m.salmanxaeed@gmail.com>
2022-01-05 14:55:08 +0500

    folder deleted single file added

---
+++ smoobu-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Smoobu Plugin
+ * Github Plugin URI: https://github.com/salmansaeed2/Smoobu
+ * Primary Branch: main
+ * Description: Manage your real estate business and market your portfolio
+ * Version: 4.0.1
+ * Author: CubixSol
+ * Author URI: https://cubixsol.com
+ * Text Domain: Smoobu
+ * @package Akismet
+ 
+ 
+ */
+
+
+
+
+ ?>
