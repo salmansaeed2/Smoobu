832e547b542979f0c5352507400e8723e08b4402
salmansaeed2 <m.salmanxaeed@gmail.com>
2022-01-05 15:01:18 +0500

    version updated

--- smoobu-plugin.php
+++ smoobu-plugin.php
@@ -4,7 +4,7 @@
  * Github Plugin URI: https://github.com/salmansaeed2/Smoobu
  * Primary Branch: main
  * Description: Manage your real estate business and market your portfolio
- * Version: 4.0.1
+ * Version: 5.0.1
  * Author: CubixSol
  * Author URI: https://cubixsol.com
  * Text Domain: Smoobu
