392d0aa42d93733ea1e4f4f83c8130e9f43f1191
salmansaeed2 <m.salmanxaeed@gmail.com>
2022-01-05 12:05:17 +0500

    version updated

--- smoobu-plugin/smoobu-plugin.php
+++ smoobu-plugin/smoobu-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Smoobu Plugin
  * Description: Manage your real estate business and market your portfolio
- * Version: 2.0.1
+ * Version: 3.0.1
  * Author: CubixSol
  * Author URI: https://cubixsol.com
  * Text Domain: Smoobu
